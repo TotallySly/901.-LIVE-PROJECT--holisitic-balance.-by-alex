c84fec0b36c05258f459f6f5da55084273cf6db6
Scott Lyon <82440653+TotallySly@users.noreply.github.com>
2023-01-07 17:59:42 +0100

    Add eventlisteners to the Font Awesome chevron icon so the small menu toggles whether it displays or not

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ const navigationLinks = document.querySelector('.navigation-links')
 hamburgerMenu.addEventListener('click', () => {
     navigationLinks.classList.toggle('display-none')
 })
+
+const chevronMenu = document.querySelector('.fa-chevron-down')
+const smallMenu = document.querySelector('.smaller-menu-items')
+
+chevronMenu.addEventListener('click', () => {
+    smallMenu.classList.toggle('display-none')
+})
