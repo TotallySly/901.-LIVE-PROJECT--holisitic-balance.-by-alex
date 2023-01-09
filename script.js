ecd4ba8802ef9d163e957c06368486b36c6c0be5
Scott Lyon <82440653+TotallySly@users.noreply.github.com>
2023-01-09 19:21:07 +0100

    Add comments explaining the JavaScript functions

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
+/**
+ * I have created a custom class called '.display-none'
+ * this class is toggled when the user clicks on the Hamburger Menu
+ */
+
 const hamburgerMenu = document.querySelector('.fa-bars')
 const navigationLinks = document.querySelector('.navigation-links')
 
 hamburgerMenu.addEventListener('click', () => {
     navigationLinks.classList.toggle('display-none')
 })
+
+/**
+ * Initially, every time the webpage loaded, the dropdown menu was
+ * automatically showing. I have manually added the '.display-none'
+ * class. This function then removes the '.display-none' class when the
+ * user clicks on the drop down.
+ *
+ * In order for it to close, I negated the contains method, it will no
+ * longer contain the '.display-none' class. As this is true, a second
+ * event listener is added, that toggles the class back and forth.
+ */
 
 const chevronMenu = document.querySelector('.fa-chevron-down')
 const dropDownMenu = document.querySelector('.smaller-menu-items')
