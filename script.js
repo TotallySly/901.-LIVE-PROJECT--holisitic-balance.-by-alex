6ee822a05235813ba55f52e43a499b8d5776462e
Scott Lyon <82440653+TotallySly@users.noreply.github.com>
2023-01-22 17:15:17 +0100

    Finsihed for the day

--- script.js
+++ script.js
@@ -109,3 +109,25 @@ arrowLeft.addEventListener('click', () => {
     changeSlides()
     resetInterval()
 })
+
+/**
+    Reveal Boxes
+ */
+
+// window.addEventListener('scroll', reveal)
+
+// function reveal() {
+//     let reveals = document.querySelectorAll('.reveal')
+
+//     for (var i = 0; i < reveals.length; i++) {
+//         let windowHeight = window.innerHeight
+//         let revealTop = reveals[i].getBoundingClientRect().top
+//         let revealPoint = 150
+
+//         if (revealTop < windowHeight - revealPoint) {
+//             reveals[i].classList.add('activeScroll')
+//         } else {
+//             reveals[i].classList.remove('activeScroll')
+//         }
+//     }
+// }
