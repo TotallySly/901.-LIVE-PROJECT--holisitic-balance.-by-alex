1fad74cc1b6bb2da9bd0ae422f1523bf24267aae
Scott Lyon <82440653+TotallySly@users.noreply.github.com>
2023-02-04 11:06:52 +0100

    Navigation links working

--- script.js
+++ script.js
@@ -135,7 +135,7 @@ function reveal() {
     for (var i = 0; i < reveals.length; i++) {
         let windowHeight = window.innerHeight
         let revealTop = reveals[i].getBoundingClientRect().top
-        let revealPoint = 150
+        let revealPoint = 3
 
         if (revealTop < windowHeight - revealPoint) {
             reveals[i].classList.add('activeScroll')
